1a2f8bf7e023dfac8c3cedc064c7469764667378
michael-small <small203@morris.umn.edu>
2021-02-11 13:54:07 -0600

    Populate generic hardcoded fields in Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import Aux from '../hocs/Aux';
+import Button from '@material-ui/core/Button';
 
 export default class Profile extends Component {
 	render() {
-		return <h1>Profile</h1>;
+		return (
+			<Aux>
+				<h1>Profile</h1>
+				<h2>Hello {'{NAME}'}</h2>
+				<Aux>
+					<p>Students: </p>
+					<ul>
+						<li>{'{STUDENT1: RANGE X-Y}'}</li>
+						<li>{'{STUDENT2: RANGE X-Y}'}</li>
+					</ul>
+					<p>Subscription: Active until {'{DATE}'}</p>
+					{/* TODO: Remove inline when I get a better hold of MUI styling */}
+					<Button style={{ border: '1px solid white' }}>
+						Renew Subscription
+					</Button>
+				</Aux>
+			</Aux>
+		);
 	}
 }
