ffcf61045f17f98f3ff496dd5b09e7e492b646b1
michael-small <small203@morris.umn.edu>
2021-02-12 13:21:44 -0600

    Use MUI `Grid` to style workshop previews
    "Previews" are still just in `Workshop`, not `WorkshopPreview`.

--- src/components/WorkshopHome/Workshop/Workshop.js
+++ src/components/WorkshopHome/Workshop/Workshop.js
@@ -1,13 +1,26 @@
+import { Grid } from '@material-ui/core';
 import React, { Component } from 'react';
 import Aux from '../../hocs/Aux';
+import './Workshop.css';
 
 export default class Workshop extends Component {
 	render() {
 		return (
 			<Aux>
-				<h1>{this.props.workshopTitle}</h1>
-				<p>{this.props.workshopPreview}</p>
-				<p>{this.props.workshopImage}</p>
+				<Grid
+					className='workshop-container'
+					justify='center'
+					alignItems='center'
+					container
+				>
+					<Grid item sm className='grid-item'>
+						<img src='https://via.placeholder.com/150' />
+					</Grid>
+					<Grid item sm>
+						<h1>{this.props.workshopTitle}</h1>
+						<p>{this.props.workshopPreview}</p>
+					</Grid>
+				</Grid>
 			</Aux>
 		);
 	}
