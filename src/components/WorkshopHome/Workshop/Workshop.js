3f4c80030026d1ecf4da1214a57ea365821d8dd9
michael-small <small203@morris.umn.edu>
2021-02-15 13:32:21 -0600

    Convert `Workshop.js` from MUI Grid to MUI Card
    These individual items were more suited to be cards, particularly
    for images. Looks wonky on mobile, but this is a better start and
    I'm sick of CSS troubles for today before work.

--- src/components/WorkshopHome/Workshop/Workshop.js
+++ src/components/WorkshopHome/Workshop/Workshop.js
@@ -1,27 +1,61 @@
-import { Grid } from '@material-ui/core';
-import React, { Component } from 'react';
-import Aux from '../../hocs/Aux';
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import './Workshop.css';
 
-export default class Workshop extends Component {
-	render() {
-		return (
-			<Aux>
-				<Grid
-					className='workshop-container'
-					justify='center'
-					alignItems='center'
-					container
-				>
-					<Grid item sm className='grid-item'>
-						<img src='https://via.placeholder.com/150' />
-					</Grid>
-					<Grid item sm>
-						<h1>{this.props.workshopTitle}</h1>
-						<p>{this.props.workshopPreview}</p>
-					</Grid>
-				</Grid>
-			</Aux>
-		);
-	}
+const useStyles = makeStyles((theme) => ({
+	card: {
+		display: 'flex',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginBottom: '1rem',
+		[theme.breakpoints.up('md')]: {
+			width: '40%',
+		},
+		padding: '4px',
+	},
+	details: {
+		display: 'flex',
+		flexDirection: 'column',
+		width: '60%',
+	},
+	content: {
+		flex: '1 0 auto',
+	},
+	image: {
+		width: '240px',
+		height: '180px',
+		[theme.breakpoints.down('md')]: {
+			width: '30%',
+			display: `block`,
+		},
+	},
+}));
+
+export default function MediaControlCard(props) {
+	const classes = useStyles();
+	const theme = useTheme();
+
+	return (
+		<Card className={classes.card}>
+			<CardMedia
+				className={classes.image}
+				image={props.workshopImage}
+				title={props.workshopTitle}
+			/>
+			<div className={classes.details}>
+				<CardContent className={classes.content}>
+					<Typography component='h5' variant='h5'>
+						{props.workshopTitle}
+					</Typography>
+					<Typography variant='subtitle1' color='textSecondary'>
+						{props.workshopPreview}
+					</Typography>
+				</CardContent>
+			</div>
+		</Card>
+	);
 }
--- src/components/WorkshopHome/WorkshopHome.js
+++ src/components/WorkshopHome/WorkshopHome.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import Aux from '../hocs/Aux';
 import Workshop from './Workshop/Workshop';
+import './WorkshopHome.css';
 export default class WorkshopHome extends Component {
 	render() {
 		return (
 			<Aux>
-				<h1>Workshops</h1>
+				<h1 id='page-title'>Workshops</h1>
 				<Workshop
-					workshopTitle='Workshop 1'
-					workshopPreview='Workshop 1 description'
-					workshopImage='workshop1_image.jpg'
+					workshopTitle='Skimming Readings for Key Points 101'
+					workshopPreview="The SAT encourages not actually reading the story and to just skim, so it's never a bad time to start learning how to do this..."
+					workshopImage='https://cdn.pixabay.com/photo/2017/04/21/02/00/book-2247427_960_720.png'
 				></Workshop>
 				<Workshop
-					workshopTitle='Workshop 2'
-					workshopPreview='Workshop 2 description'
-					workshopImage='workshop2_image.jpg'
+					workshopTitle='Writing Cleanly'
+					workshopPreview="You won't change any minds if your points are incredible but your writing is illegible. Learn tips on writing well."
+					workshopImage='https://cdn.pixabay.com/photo/2017/05/03/22/08/notebook-2282300_960_720.png'
 				></Workshop>
 			</Aux>
 		);
