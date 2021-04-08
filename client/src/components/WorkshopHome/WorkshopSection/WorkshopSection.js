844707ce9b7820fb6d58bf42889ef0987ead7b21
michael-small <small203@morris.umn.edu>
2021-04-08 12:55:49 -0500

    Retrieve a workshop section
    Addresses #5 (Pull Section data from GraphCMS)
    Project: 'Create Workshop Lessons Listing'

--- client/src/components/WorkshopHome/WorkshopSection/WorkshopSection.js
+++ client/src/components/WorkshopHome/WorkshopSection/WorkshopSection.js
@@ -1,25 +1,32 @@
 import React from 'react';
-
+import Aux from '../../hocs/Aux';
 import { gql, useQuery } from '@apollo/client';
 
-const GET_WORKSHOPS = gql`
-	query GetWorkshops {
-		workshops {
-			id
-			sections {
-				lessons {
-					lessonTitle
-				}
-				sectionTitle
+const GET_SECTIONS = gql`
+	query GetSections {
+		sections {
+			lessons {
+				lessonTitle
 			}
-			workshopTitle
-			previewPhoto {
-				url
-			}
+			sectionTitle
 		}
 	}
 `;
 
 export default function WorkshopSection() {
-	return <h1>WorkshopSection</h1>;
+	const { loading, error, data } = useQuery(GET_SECTIONS);
+
+	if (loading) return 'Loading...';
+	if (error) return `Error! ${error.message}`;
+	return (
+		<Aux>
+			<h1>WorkshopSection</h1>
+			{data.sections.map((section, index) => (
+				<div key={index}>
+					<h2>{section.sectionTitle}</h2>
+				</div>
+			))}
+			<h2></h2>
+		</Aux>
+	);
 }
