8d170815eb17c74d238d06f5b86e384a6f660754
michael-small <small203@morris.umn.edu>
2021-04-08 12:46:27 -0500

    Pull workshop previews from GraphCMS
    Addresses #3 (Pull Workshop preview info from GraphCMS)
    Project: 'Create Workshop Lessons Listing'

--- client/src/components/WorkshopHome/WorkshopHome.js
+++ client/src/components/WorkshopHome/WorkshopHome.js
@@ -1,23 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../hocs/Aux';
 import Workshop from './Workshop/Workshop';
 import './WorkshopHome.css';
-export default class WorkshopHome extends Component {
-	render() {
-		return (
-			<Aux>
-				<h1 id='page-title'>Workshops</h1>
-				<Workshop
-					workshopTitle='Skimming Readings for Key Points 101'
-					workshopPreview="The SAT encourages not actually reading the story and to just skim, so it's never a bad time to start learning how to do this..."
-					workshopImage='https://cdn.pixabay.com/photo/2017/04/21/02/00/book-2247427_960_720.png'
-				></Workshop>
-				<Workshop
-					workshopTitle='Writing Cleanly'
-					workshopPreview="You won't change any minds if your points are incredible but your writing is illegible. Learn tips on writing well."
-					workshopImage='https://cdn.pixabay.com/photo/2017/05/03/22/08/notebook-2282300_960_720.png'
-				></Workshop>
-			</Aux>
-		);
+
+import { gql, useQuery } from '@apollo/client';
+
+const GET_WORKSHOPS = gql`
+	query GetWorkshops {
+		workshops {
+			id
+			sections {
+				lessons {
+					lessonTitle
+				}
+				sectionTitle
+			}
+			workshopTitle
+			previewPhoto {
+				url
+			}
+		}
 	}
+`;
+export default function WorkshopHome() {
+	const { loading, error, data } = useQuery(GET_WORKSHOPS);
+
+	if (loading) return 'Loading...';
+	if (error) return `Error! ${error.message}`;
+
+	return (
+		<Aux>
+			<h1 id='page-title'>Workshops</h1>
+			{data.workshops.map((workshop, key) => (
+				<Workshop
+					key={workshop.id}
+					workshopTitle={workshop.workshopTitle}
+					workshopPreview={workshop.workshopPreview}
+					workshopImage={workshop.previewPhoto.url}
+				/>
+			))}
+		</Aux>
+	);
 }
